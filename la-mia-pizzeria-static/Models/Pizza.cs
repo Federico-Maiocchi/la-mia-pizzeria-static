a53815a39e300605544410c1ec1852d8fe551a63
Federico Maiocchi <maiocchifederico@gmail.com>
2024-05-10 14:59:38 +0200

    Aggiungere i file di progetto.

---
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -0,0 +1,21 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public string Nome { get; set; }
+
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+
+        public decimal Price { get; set; }
+
+        public Pizza(string nome, string description, string image, decimal price)
+        {
+            Nome = nome;
+            Description = description;
+            Image = image;
+            Price = price;
+        }
+    }
+}
